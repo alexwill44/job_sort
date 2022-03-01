88ad3cd43cdd535ecd8769b325200e4c4aad73a3
alexwill44 <AlArvanitis@gmail.com>
2022-02-28 17:51:57 -0800

    fixed the get_by_od route so it is now much more proformant

--- server/api/crud/job.py
+++ server/api/crud/job.py
@@ -16,8 +16,8 @@ class JobCrud:
         global CACHE
         try: 
             async with db as session:
-                result = await session.execute(select(Job))
-                CACHE = {i.id: i.title for i in result.scalars() if i.id == id}
+                result = await session.execute(select(Job).where(Job.id == id))
+                CACHE = {i.id: i.title for i in result.scalars()}
                 return CACHE
         except: 
             print(error)
